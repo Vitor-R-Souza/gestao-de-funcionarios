1ec9fa682a69df60d692c3a30b0ddd5e52bb2996
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2025-02-26 15:21:43 -0300

    Initial commit

---
+++ src/main/java/com/personal/gestao_de_funcionarios/GestaoDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.personal.gestao_de_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeFuncionariosApplication.class, args);
+	}
+
+}
